34fa7f957d2692eccc4ad5350efda70b1025e7b6
Brooke McFarland <brookelyn.mcf@gmail.com>
2016-04-24 17:30:00 -0500

    Create functions for creating table and prompt to get user input for what order the data is in

--- program #7.py
+++ program #7.py
@@ -26,25 +26,47 @@ def get_user_input(question):
         try:
             correct = open_file(answer)
         except FileNotFoundError:
-            print ("The file",answer,"was not found. Please try again.")
+            print ("The file",answer,"was not found; Please try again.")
         except IOError:
             print("Please check the file name",answer,"for any incorrect special characters")
+        except Exception:
+            print("Something went wrong; Please try again.")
         else:
             return correct
+
+def table_input(question):
+    user_answer = input(question)
+
+#def make_table():
+    #answer = input(order)
+
+
+
 
 class Word(object):
     def __init__(self,word):
         self.word = word
-        self.rating_review_lst = []
+        self.rating_lst = []
+        self.rating = 0
+        self. word_count = 0
     def avg_rating(self):
-        total_rating = 0
-        for number in self.rating_review_lst:
-            total_rating += number
-        if len(self.rating_review_lst) > 0:
-            return total_rating/len(self.rating_review_lst)
+        if len(self.rating_lst) > 0:
+            return self.total_rating()/len(self.rating_lst)
         else:
             return 0
-    #def st_deviation(self):
+    def total_rating(self):
+        total_rating = 0
+        for number in self.rating_lst:
+            total_rating += number
+        return total_rating
+
+    def st_deviation(self):
+        avg = self.avg_rating()
+        sum_numerator = 0
+        for number in self.rating_lst:
+            sum_numerator += ((number-avg)**2)
+        return sum_numerator/len(self.rating_lst)
+
 
 
 class Review(object):
@@ -63,6 +85,7 @@ while True:
 
 
     word_file = get_user_input("Please enter the file containing a list of words you would like to analyze.")
+    order_question = table_input("In which order would you like your data displayed?\n1.)Avg ascending\n2.)Avg descending\n3.)Standard deviation ascending\n4.)Standard deviation descending\n")
     word_lst = []
     for item in word_file:
         movie_word = Word((" "+(item.strip("\n"))+" ").lower())
@@ -70,5 +93,6 @@ while True:
     for review in review_lst:
         for word in word_lst:
             if word.word in review.review:
-                word.rating_review_lst.append(review.rating)
-    print("Fart")
+                word.rating_lst.append(review.rating)
+
+    print("-"*75)
